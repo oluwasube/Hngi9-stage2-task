d218335fafaef49b6639a6f803efd983d6f25b89
oluwasube <oluwasube@outlook.com>
2022-11-05 17:37:23 +0100

    task updated

--- app.py
+++ app.py
@@ -17,20 +17,20 @@ def post_computation():
       # Unpacking the data posted to the API
     
     operation_type= request.json['operation_type']
-    x =request.json['x']
-    y = request.json['y']
+    x =int(request.json['x'])
+    y = int(request.json['y'])
       
     result =0
-    if operation_type == "minus" or operation_type == "substract" or operation_type == "substraction":
+    if operation_type == "minus" or operation_type == "subtract" or operation_type == "subtraction":
         result = x - y
-    if operation_type == "add" or operation_type == "addition":
+    if operation_type == "add" or operation_type == "addition" or operation_type == "plus":
         result = x + y
     if operation_type == "multiply" or operation_type == "multiplication":
         result = x * y
     return jsonify({
             'slackUsername': 'oluwasube',
             'result': result,
-            'tion_type': operation_typeopera
+            'operation_type': operation_type
         })
     
 if __name__ == '__main__':
